581569e56ca5e0ccc2334f11d44e0500983ee197
Noah Moroze <noahmoroze@gmail.com>
2024-06-30 19:37:48 -0700

    factor our useful code from tclint entry point

--- src/tclint/tclint.py
+++ src/tclint/tclint.py
@@ -1,20 +1,17 @@
 """Main CLI entry point."""
 
 import argparse
-import codecs
-import os
 import pathlib
-import re
 import sys
 from typing import List, Optional
 
-import pathspec
 
 from tclint.config import get_config, setup_config_cli_args, Config, ConfigError
 from tclint.parser import Parser, TclSyntaxError
 from tclint.checks import get_checkers
 from tclint.violations import Violation, Rule
 from tclint.comments import CommentVisitor
+from tclint.cli.utils import resolve_sources, register_codec_warning
 from tclint import utils
 
 try:
@@ -27,72 +24,6 @@ EXIT_OK = 0
 EXIT_LINT_VIOLATIONS = 1
 EXIT_SYNTAX_ERROR = 2
 EXIT_INPUT_ERROR = 4
-
-
-def resolve_sources(
-    paths: List[pathlib.Path],
-    exclude_patterns: List[str],
-    exclude_root: pathlib.Path,
-    extensions: List[str],
-) -> List[Optional[pathlib.Path]]:
-    """Resolves paths passed via CLI to a list of filepaths to lint.
-
-    `paths` is a list of paths that may be files or directories. Files are
-    returned verbatim if they exist, and directories are recursively searched
-    for files that have an extension specified in `extensions`. Paths that match a
-    pattern in `exclude_patterns` are ignored (based on gitignore pattern
-    format, see https://git-scm.com/docs/gitignore#_pattern_format).
-
-    Raises FileNotFoundError if a supplied path does not exist.
-    """
-    extensions = [f".{ext}" if not ext.startswith(".") else ext for ext in extensions]
-    exclude_root = exclude_root.resolve()
-    exclude_patterns = [
-        re.sub(r"^\s*#", r"\#", pattern) for pattern in exclude_patterns
-    ]
-    exclude_spec = pathspec.PathSpec.from_lines("gitwildmatch", exclude_patterns)
-
-    def is_excluded(path):
-        abspath = path.resolve()
-        try:
-            relpath = os.path.relpath(abspath, start=exclude_root)
-        except ValueError:
-            print(
-                "Warning: processing files on different drive from where tclint was"
-                " run, 'exclude' config may not behave as expected"
-            )
-            relpath = abspath
-
-        if exclude_spec.match_file(relpath):
-            return True
-        return False
-
-    sources: List[Optional[pathlib.Path]] = []
-
-    for path in paths:
-        if str(path) == "-":
-            sources.append(None)
-            continue
-
-        if not path.exists():
-            raise FileNotFoundError(f"path {path} does not exist")
-
-        if is_excluded(path):
-            continue
-
-        if not path.is_dir():
-            sources.append(path)
-            continue
-
-        for dirpath, _, filenames in os.walk(path):
-            for name in filenames:
-                _, ext = os.path.splitext(name)
-                if ext.lower() in extensions:
-                    child = pathlib.Path(dirpath) / name
-                    if not is_excluded(child):
-                        sources.append(child)
-
-    return sources
 
 
 def filter_violations(
@@ -144,15 +75,6 @@ def lint(
     violations = filter_violations(violations, config.ignore, ignore_lines, path)
 
     return violations
-
-
-def replace_with_warning_handler(e):
-    # TODO: formal warning mechanism, include path
-    print("Warning: non-unicode characters in file, replacing with �")
-    return codecs.replace_errors(e)
-
-
-codecs.register_error("replace_with_warning", replace_with_warning_handler)
 
 
 def main():
@@ -216,6 +138,8 @@ def main():
         return EXIT_INPUT_ERROR
 
     retcode = EXIT_OK
+
+    register_codec_warning("replace_with_warning")
 
     for path in sources:
         if path is None:
