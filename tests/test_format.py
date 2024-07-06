917d410eb9d59a1b19358a76376eac54f1e5a48d
Noah Moroze <noahmoroze@gmail.com>
2024-07-05 23:32:47 -0400

    format: return lists of indentable lines
    This commit refactors each format function to return its formatted
    content as a list of indentable lines, rather than strings.
    
    This means, for example, that a multi-line string that cannot have
    indents inserted within it ends up being one entry in this list.
    
    Note: this commit fails tests.

--- tests/test_format.py
+++ tests/test_format.py
@@ -8,6 +8,15 @@ from tclint.parser import Parser
 MY_DIR = pathlib.Path(__file__).parent.resolve()
 
 
+def _test(script, expected):
+    parser = Parser()
+    tree = parser.parse(script)
+    format = Formatter()
+    out = format.format_top(tree)
+
+    assert out == expected
+
+
 def test_fizzbuzz():
     with open(MY_DIR / "data" / "dirty.tcl", "r") as f:
         script = f.read()
@@ -36,10 +45,18 @@ def test_fizzbuzz():
 def test_blank_lines():
     """Formatter preserves up to two blank lines."""
     TREE = Script(
-        Command(BareWord("foo"), pos=(1, 1), end_pos=(1, 4)),
-        Command(BareWord("foo"), pos=(3, 1), end_pos=(3, 4)),
-        Command(BareWord("foo"), pos=(6, 1), end_pos=(6, 4)),
-        Command(BareWord("foo"), pos=(10, 1), end_pos=(10, 4)),
+        Command(
+            BareWord("foo", pos=(1, 1), end_pos=(1, 4)), pos=(1, 1), end_pos=(1, 4)
+        ),
+        Command(
+            BareWord("foo", pos=(3, 1), end_pos=(3, 4)), pos=(3, 1), end_pos=(3, 4)
+        ),
+        Command(
+            BareWord("foo", pos=(6, 1), end_pos=(6, 4)), pos=(6, 1), end_pos=(6, 4)
+        ),
+        Command(
+            BareWord("foo", pos=(10, 1), end_pos=(10, 4)), pos=(10, 1), end_pos=(10, 4)
+        ),
     )
 
     expected = "foo\n\nfoo\n\n\nfoo\n\n\nfoo"
@@ -50,8 +67,12 @@ def test_blank_lines():
 def test_multiple_cmds_per_line():
     """Formatter preserves commands on same line."""
     TREE = Script(
-        Command(BareWord("foo"), pos=(1, 1), end_pos=(1, 4)),
-        Command(BareWord("foo"), pos=(1, 5), end_pos=(1, 8)),
+        Command(
+            BareWord("foo", pos=(1, 1), end_pos=(1, 4)), pos=(1, 1), end_pos=(1, 4)
+        ),
+        Command(
+            BareWord("foo", pos=(1, 5), end_pos=(1, 8)), pos=(1, 5), end_pos=(1, 8)
+        ),
     )
     expected = "foo; foo"
 
@@ -122,3 +143,149 @@ def test_switch():
 }"""
 
     assert Formatter().format_top(TREE) == expected
+
+
+def test_no_reindent_braced_word():
+    """We can't add an extra level of indent in the second line of the braced word,
+    since this will change the actual text."""
+    script = r"""
+puts \
+{ one
+  two }
+"""
+
+    parser = Parser()
+    tree = parser.parse(script)
+
+    EXPECTED = r"""
+puts \
+  { one
+  two }""".strip()
+
+    format = Formatter()
+    out = format.format_top(tree)
+
+    assert out == EXPECTED
+
+
+def test_no_reindent_braced_word_script():
+    """We can't add an extra level of indent in the second line of the braced word,
+    since this will change the actual text."""
+    script = r"""
+if { 1 } {
+puts { one
+  two }
+}"""
+
+    expected = r"""
+if { 1 } {
+  puts { one
+  two }
+}""".strip()
+
+    _test(script, expected)
+
+
+def test_reindent_command_sub():
+    script = r"""
+puts [command \
+foo]"""
+
+    parser = Parser()
+    tree = parser.parse(script)
+
+    EXPECTED = r"""
+puts [command \
+        foo]
+""".strip()
+
+    format = Formatter()
+    out = format.format_top(tree)
+
+    assert out == EXPECTED
+
+
+def test_reindent_command_sub_new_line():
+    script = r"""
+puts \
+[command \
+foo]"""
+
+    parser = Parser()
+    tree = parser.parse(script)
+
+    EXPECTED = r"""
+puts \
+  [command \
+    foo]
+""".strip()
+
+    format = Formatter()
+    out = format.format_top(tree)
+
+    assert out == EXPECTED
+
+
+def test_expr_align():
+    script = r"""
+if {$a &&
+$b } { puts "asdf" }"""
+
+    expected = r"""
+if {$a &&
+    $b } { puts "asdf" }""".strip()
+
+    _test(script, expected)
+
+
+def test_expr_command_sub_alignment():
+    script = r"""
+if { ![command $arg1 \
+    $arg2 \
+    $arg3] } {
+    return true
+}"""
+
+    expected = r"""
+if { ![command $arg1 \
+         $arg2 \
+         $arg3] } {
+    return true
+}"""
+
+    _test(script, expected)
+
+
+def test_indent_bare_word_command_sub():
+    script = r"""
+puts foo[bar\
+    "mootown"]qwerty"""
+
+    expected = r"""
+puts foo[bar \
+           "mootown"]qwerty
+""".strip()
+
+    _test(script, expected)
+
+
+def test_varsub_index_preserve_newline():
+    script = r"""i
+puts $foo(asdf \
+asdf)""".strip()
+
+    _test(script, script)
+
+
+def test_varsub_indent_format():
+    script = r"""
+$foo(asdf$asdf[asdf \
+-asdf] [qwerty \
+-uiop])"""
+
+    expected = r"""
+$foo(asdf$asdf[asdf \
+                 -asdf] [qwerty \
+                           -uiop])""".strip()
+
+    _test(script, expected)
