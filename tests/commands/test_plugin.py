ef36e4d6c23ac3fe24f4e36992b9873f159097b6
Noah Moroze <noahmoroze@gmail.com>
2025-03-30 12:04:14 -0400

    tests: check in OpenROAD command spec
    A future commit is going to strip out the OpenROAD plugin support and
    make its command spec format a first-class part of the codebase. This
    gives us a test case for working on the command spec checking. We can
    update it as the spec evolves via a migration script.
    
    This spec was generated from OpenROAD v2.0-13652-g6fc686431.

---
+++ tests/commands/test_plugin.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+# Not clean, but we import the _PluginManager class instead of using the singleton so we
+# have isolation between tests.
+from tclint.commands.plugins import _PluginManager as PluginManager
+
+MY_DIR = Path(__file__).parent.resolve()
+TEST_DATA_DIR = MY_DIR / "data"
+
+
+def test_load_openroad():
+    plugins = PluginManager()
+    loaded = plugins.load_from_spec(TEST_DATA_DIR / "openroad.json")
+    assert isinstance(loaded, dict)
+
+
+def test_load_invalid():
+    plugins = PluginManager()
+    loaded = plugins.load_from_spec(TEST_DATA_DIR / "invalid.json")
+    assert loaded is None
