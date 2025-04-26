8a40d871843944fe894f30c57063d9eaf0625daf
Noah Moroze <noahmoroze@gmail.com>
2025-04-26 17:41:41 -0400

    tests: add config validation tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,8 +1,15 @@
+import argparse
 import pathlib
 
 import pytest
 
-from tclint.config import get_config, RunConfig, ConfigError
+from tclint.config import (
+    get_config,
+    RunConfig,
+    ConfigError,
+    setup_config_cli_args,
+    setup_tclfmt_config_cli_args,
+)
 from tclint.violations import Rule
 
 MY_DIR = pathlib.Path(__file__).parent.resolve()
@@ -47,3 +54,119 @@ def test_pyproject():
     global_ = config.get_for_path(pathlib.Path())
     assert global_.style_indent == 2
     assert global_.ignore == [Rule("unbraced-expr")]
+
+
+def test_tclint_config_args():
+    parser = argparse.ArgumentParser("tclint")
+    setup_config_cli_args(parser)
+
+    args = [
+        "--ignore",
+        "unbraced-expr, line-length",
+        "--extend-ignore",
+        "trailing-whitespace",
+        "--exclude",
+        "my_dir,my_file",
+        "--extend-exclude",
+        "extend_to_file",
+        "--extensions",
+        "sdc, exp",
+        "--commands",
+        "commands.json",
+        "--style-line-length",
+        "79",
+    ]
+
+    args = parser.parse_args(args)
+
+    assert args.ignore == [Rule("unbraced-expr"), Rule("line-length")]
+    assert args.extend_ignore == [Rule("trailing-whitespace")]
+    assert args.exclude == ["my_dir", "my_file"]
+    assert args.extend_exclude == ["extend_to_file"]
+    assert args.extensions == ["sdc", "exp"]
+    assert args.commands == pathlib.Path("commands.json")
+    assert args.style_line_length == 79
+
+
+def test_tclfmt_config_args():
+    parser = argparse.ArgumentParser("tclfmt")
+    setup_tclfmt_config_cli_args(parser)
+
+    args = [
+        "--exclude",
+        "my_dir,my_file",
+        "--extend-exclude",
+        "extend_to_file",
+        "--extensions",
+        "sdc, exp",
+        "--commands",
+        "commands.json",
+        "--indent",
+        "5",
+        "--max-blank-lines",
+        "4",
+        "--indent-namespace-eval",
+        "--no-spaces-in-braces",
+    ]
+
+    args = parser.parse_args(args)
+
+    assert args.exclude == ["my_dir", "my_file"]
+    assert args.extend_exclude == ["extend_to_file"]
+    assert args.extensions == ["sdc", "exp"]
+    assert args.commands == pathlib.Path("commands.json")
+    assert args.style_indent == 5
+    assert args.style_max_blank_lines == 4
+    assert args.style_indent_namespace_eval is True
+    assert args.style_spaces_in_braces is False
+
+
+def test_invalid_tclint_args():
+    parser = argparse.ArgumentParser("tclint", exit_on_error=False)
+    setup_config_cli_args(parser)
+
+    for arg, value in [
+        ("--ignore", "unbraced-expr, line-length, invalid-rule"),
+        ("--extend-ignore", "trailing-whitespace, invalid-rule"),
+        ("--style-line-length", "asdf"),
+    ]:
+        with pytest.raises(argparse.ArgumentError) as excinfo:
+            parser.parse_args([arg, value])
+
+        # For manually auditing error messages
+        print(excinfo.value)
+
+
+def test_invalid_tclfmt_args():
+    parser = argparse.ArgumentParser("tclfmt", exit_on_error=False)
+    setup_tclfmt_config_cli_args(parser)
+
+    for arg, value in [
+        ("--indent", "qwerty"),
+        ("--max-blank-lines", "uiop"),
+    ]:
+        with pytest.raises(argparse.ArgumentError) as excinfo:
+            parser.parse_args([arg, value])
+
+        # For manually auditing error messages
+        print(excinfo.value)
+
+
+def test_invalid_config():
+    for config in [
+        {"ignore": ["unbraced-expr", "invalid-rule", "line-length"]},
+        {"commands": 1},
+        {"style": {"indent": "abc"}},
+        {"style": {"line-length": "def"}},
+        {"style": {"max-blank-lines": "ghi"}},
+        {"style": {"indent-namespace-eval": "not-a-bool"}},
+        {"style": {"spaces-in-braces": -1}},
+        {"fileset": [{"style": {"indent": 4}}]},  # missing key
+        {"fileset": [{"paths": ["dir"], "style": {"indent": "not-a-number"}}]},
+        {"extra-key": "value"},
+    ]:
+        with pytest.raises(ConfigError) as excinfo:
+            RunConfig.from_dict(config, pathlib.Path.cwd())
+
+        # For manually auditing error messages
+        print(excinfo.value)
