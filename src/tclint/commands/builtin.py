730bc16a7c2c91a2c3ba3cf3a307f2d83d0b2d65
Noah Moroze <noahmoroze@gmail.com>
2025-12-30 23:05:24 -0500

    Revert "checks: replace check_count with no args with static spec"
    This reverts commit 848f18bc1045e446496f18e324f918e8e99aacc6.
    
    This was totally wrong, no arguments means an unrestricted number of
    args, not no args... whoops

--- src/tclint/commands/builtin.py
+++ src/tclint/commands/builtin.py
@@ -938,7 +938,7 @@ commands = commands_schema({
     },
     "chan": _chan,
     # TODO: check subcommands
-    "clock": {},
+    "clock": check_count("clock"),
     "close": {
         "positionals": [
             {"name": "channelId", "value": {"type": "any"}, "required": True},
@@ -961,7 +961,7 @@ commands = commands_schema({
     "dict": {
         "subcommands": {
             "append": check_count("dict append", 2, None),
-            "create": {},
+            "create": check_count("dict create"),
             "exists": check_count("dict exists", 2, None),
             "filter": _dict_filter,
             "for": _dict_map_for("dict for"),
@@ -971,7 +971,7 @@ commands = commands_schema({
             "keys": check_count("dict keys", 1, 2),
             "lappend": check_count("dict lappend", 2, None),
             "map": _dict_map_for("dict map"),
-            "merge": {},
+            "merge": check_count("dict merge"),
             "remove": check_count("dict remove", 1, None),
             "replace": check_count("dict replace", 1, None),
             "set": check_count("dict set", 3, None),
@@ -1018,9 +1018,9 @@ commands = commands_schema({
     "foreach": foreach,
     "format": check_count("format", 1, None),
     "gets": check_count("gets", 1, 2),
-    "glob": {},
-    "global": {},
-    "history": {},
+    "glob": check_count("glob"),
+    "global": check_count("global"),
+    "history": check_count("history"),
     "if": _if,
     "incr": check_count("incr", 1, 2),
     # TODO: check subcommands
@@ -1170,17 +1170,17 @@ commands = commands_schema({
     "timerate": _timerate,
     "tcl::tm::path": {
         "subcommands": {
-            "add": {},
-            "remove": {},
+            "add": check_count("tcl::tm::path add"),
+            "remove": check_count("tcl::tm::path remove"),
             "list": check_count("tcl::tm::path list", 0, 0),
         },
     },
-    "tcl::tm::roots": {},
+    "tcl::tm::roots": check_count("tcl::tm::roots"),
     # TODO: check subcommands
     "trace": check_count("trace", 2, None),
     "try": _try,
     "unload": check_count("unload", 1, 6),
-    "unset": {},
+    "unset": check_count("unset"),
     "update": check_count("update", 0, 1),
     "uplevel": check_count("uplevel", 1, None),
     "upvar": check_count("upvar", 2, None),
